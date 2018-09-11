bf9d5f61a60883ac6d0679d4f28b23724e9aa4da
jacob-anabi <anabi@chapman.edu>
2018-09-11 18:03:27 +0000

    fixed range in for loop

--- sequences.py
+++ sequences.py
@@ -8,9 +8,9 @@ def fibonacci(n):
     firstfib = 1
     secondfib = 1
     temp = 0
-    seq = ""
-    for i in range (n+1):
-        seq += (firstfib + " ")
+    seq = []
+    for i in range (n):
+        seq.append(firstfib)
         temp = secondfib + firstfib
         firstfib = secondfib
         secondfib = temp
