bcf43ae1ceb045761638c0334f5070b33f80741b
jacob-anabi <anabi@chapman.edu>
2018-09-11 17:52:15 +0000

    added sequences.py module

---
+++ sequences.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def fibonacci(n):
+    """ returns the sequence of n fibonacci numbers
+    """
+    firstfib = 1
+    secondfib = 1
+    temp = 0
+    seq = ""
+    for i in range (n+1):
+        seq += (firstfib + " ")
+        temp = secondfib + firstfib
+        firstfib = secondfib
+        secondfib = temp
+return seq
+    
