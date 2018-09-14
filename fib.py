8401b191ac20da3697344977dc1f820c67cb583e
jacob-anabi <anabi@chapman.edu>
2018-09-14 07:02:31 +0000

    added fib.py python script

---
+++ fib.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sequences as sq
+
+def main(argv):
+    try:
+        fib_seq = sq.fibonacci(int(sys.argv[1]))
+        print(fib_seq[-1], end = ', ')
+    except Exception as e:
+        print (e)
+    
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
