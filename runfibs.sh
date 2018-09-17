4d83d0f74a02774ba8bb8367c8100dcd885c5412
jacob-anabi <anabi@chapman.edu>
2018-09-17 20:38:41 +0000

    added check for fibs.csv and fibs.csv.bak already existing

--- runfibs.sh
+++ runfibs.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ -e fibs.csv ]
+then
+    if [ -e fibs.csv.bak ]
+    then
+        echo "fibs.csv.bak already exists"
+        exit 1
+    else
+        mv fibs.csv fibs.csv.bak
+        echo "Backing up fibs.csv to fibs.csv.bak, overwriting fibs.csv"
+    fi
+fi
+     
 for i in $(seq 10000); do
     ./fib.py ${i} >> fibs.csv
 done
