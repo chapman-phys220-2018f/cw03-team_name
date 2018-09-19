28399b50c5f0de161cd5dc72a88772dfffd63dc5
Monica Hiemer <hiemer@chapman.edu>
2018-09-19 22:24:45 +0000

    fixed m

--- fib.py
+++ fib.py
@@ -5,7 +5,7 @@ import sequences as sq
 def main(argv):
     try:
         fib_seq = sq.fibonacci(int(sys.argv[1]))
-        print(fib_seq[-1], end = ', ')
+        print(fib_seq[-])
     except Exception as e:
         print (e)
     
